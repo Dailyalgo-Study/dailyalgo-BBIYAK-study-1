920ecef0a2fe5af3b258b27df8ff3692361ca351
PEANUTBUTTER1001 <yangjk9203@naver.com>
2025-11-02 20:29:36 +0900

    양정규 1주체 과제 완료 (#10)

---
+++ 양정규/1주차/01_두_수의_합/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int solution(int[] numbers, int m) {
+        int answer = 0;
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                sum = numbers[i] + numbers[j];
+                if (sum <= m && sum >= answer) {
+                    answer = sum;
+                }
+            }
+        }
+        return answer;
+    }
+}
