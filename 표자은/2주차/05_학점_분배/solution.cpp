7cb5256200b603a2c8138ed1f17ad30153473d0b
jjani0 <56445883+jjani0@users.noreply.github.com>
2025-11-09 22:26:21 +0900

    표자은 2주차 (#12)
    * feat: sol for 06-10
    
    * feat: sol for 01, 03-05
    
    * feat: sol for 05

---
+++ 표자은/2주차/05_학점_분배/solution.cpp
@@ -0,0 +1,50 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<int> scores) {
+    // 인덱스 배열을 만든다
+    int n = scores.size();
+    vector<string> answer(n);
+    vector<int> indexs(n);
+    for (int i = 0; i < n; i++)
+    {
+        indexs[i] = i;
+    }
+
+    // score-index pair vector 만든다
+    vector<pair<int, int>> sipair;
+    for (int i = 0; i < n; i++)
+    {
+        sipair.push_back({ scores[i], indexs[i] });
+    }
+
+    // 정렬한다
+    sort(sipair.begin(), sipair.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+        if (a.first == b.first)
+        {
+            return a.second < b.second;
+        }
+        return a.first > b.first;
+        });
+
+    // 인덱스대로 점수 매핑 해준다
+    for (int i = 0; i < n; i++)
+    {
+        int idx = sipair[i].second;
+        if (i < (n / 3))
+        {
+            answer[idx] = "A";
+        }
+        else if (i < (2 * n / 3))
+        {
+            answer[idx] = "B";
+        }
+        else
+        {
+            answer[idx] = "C";
+        }
+    }
+    return answer;
+}
