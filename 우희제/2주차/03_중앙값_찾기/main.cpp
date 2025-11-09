b1252df6a866157b965849a86c25758aba2051be
우희제 <116641382+wooheeje@users.noreply.github.com>
2025-11-09 22:28:07 +0900

    우희제 2주차 과제 수행 완료 (#15)
    * 2주차 과제 수행 완료
    
    4문제 못 풀었습니다.
    
    * 1주차 과제 추가
    
    ---------
    
    Co-authored-by: KIM_KIBUM <53718787+kibum99@users.noreply.github.com>

---
+++ 우희제/2주차/03_중앙값_찾기/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> numbers)
+{
+    int index = numbers.size() / 2;
+
+    sort(numbers.begin(), numbers.end());
+
+    return numbers[numbers.size() / 2];
+}
